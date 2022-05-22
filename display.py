5b9e99b668537387040f9ce28a8d7d301f65bfcb
jun6000 <arjunvishanth@gmail.com>
2022-05-22 20:32:42 +0530

    First commit to GitHub.

---
+++ display.py
@@ -0,0 +1,15 @@
+import json, os, sys, time
+
+def print_gradual (str, timeout): # Print character by character with a time delay
+    for c in str:
+        sys.stdout.write (c)
+        sys.stdout.flush ()
+        time.sleep (timeout)
+
+with open ("ogura.json", "r") as p:
+    data = json.load (p)
+
+print_gradual (data["japanese"][0]["line"], 0.1)
+print ("\n")
+print_gradual (data["english"][0]["line"], 0.05)
+print ("\n")
