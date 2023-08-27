cfc7f9c1438929f541a4f2566b5c29c30c75102c
Arjun Vishanth <arjunvishanth@gmail.com>
2023-08-27 14:50:48 +0530

    Added __main__.py to run ogura module using python -m.

--- src/ogura/display.py
+++ src/ogura/display.py
@@ -1,22 +1,23 @@
 import json, sys, time, random, pkg_resources
 
-def bold(str): # Returns str in bold
+
+def bold(str):
     return "\033[1m" + str + "\033[0m"
 
-def print_gradual(str, timeout): # Print character by character with a time delay
+
+def print_gradual(str, timeout):
     for c in str:
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(timeout)
 
-with open(pkg_resources.resource_filename(__name__, "ogura.json"), "r") as p:
-    data = json.load(p)
 
-i = random.randint(0, 99)
-print()
-print_gradual(data["japanese"][i]["line"], 0.05)
-print()
-print_gradual(data["romaji"][i]["line"], 0.03)
-print()
-print_gradual(data["english"][i]["line"], 0.03)
-print()
+def main():
+    with open(pkg_resources.resource_filename(__name__, "ogura.json"), "r") as p:
+        data = json.load(p)
+
+    i = random.randint(0, 99)
+    print()
+    print_gradual(data["japanese"][i]["line"] + "\n", 0.05)
+    print_gradual(data["romaji"][i]["line"] + "\n", 0.03)
+    print_gradual(data["english"][i]["line"] + "\n", 0.03)
